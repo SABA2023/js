2c0f0049f4b06baed0f50f4a7d12fabf23711398
SABA2023 <125206577+SABA2023@users.noreply.github.com>
2023-02-13 23:31:15 +0800

    Update sabasg.js

--- sabasg.js
+++ sabasg.js
@@ -1,8 +1,12 @@
 <script>
+   Ecwid.OnAPILoaded.add(function() {
+    console.log("Storefront JS API has loaded");
+});
+
    Ecwid.OnPageLoaded.add(function(page) {
     if (page.type == "CART") {
       // Print Page Type
-       console.log("Current page is of type: " + page.type);
+       console.log("Page Type: " + page.type);
     }
 });
 </script>
