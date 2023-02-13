95ed0a92e6ebe9d3b5a00d753d1874c6f88d37eb
SABA2023 <125206577+SABA2023@users.noreply.github.com>
2023-02-13 22:33:24 +0800

    Update sabasg.js

--- sabasg.js
+++ sabasg.js
@@ -1,36 +1,8 @@
 <script>
-Ecwid.OnPageLoaded.add(function(page) {
-   	setTimeout(function hidePrice() {
-   		$(".ecwid-productBrowser-price").each(function(idx, el){
-           if ($(el).text() == Ecwid.formatCurrency(0)) {
-           		if(page.type == "CATEGORY") {
-           			$(el).text('Call for price');
-           			$('.ecwid-BuyNow-outOfStockLabel').hide();
-           		}
-           		else if (page.type == "PRODUCT") {
-  					$(this).text('Call for price');
-  					$('.ecwid-productBrowser-details-inStockLabel').show();
-  					$('.ecwid-productBrowser-details-outOfStockLabel').hide();
-  					$('.ecwid-AddToBagButton').hide();
-  				}
-            } 
-            else {
-            	$('.ecwid-productBrowser-details-outOfStockLabel').show();
-  				$('.ecwid-AddToBagButton').show();
-            }
-     	});
-   	}, 50);
-   
-    ecwidMessages = {
-    "BreadCrumbs.your_bag" : "My shopping cart",
-    "Minicart.shopping_bag" : "Shopping Cart.",
-    "ShoppingCartView.shopping_bag" : "My Shopping Cart,",
-    "EmptyShoppingCartPanel.empty" : "My Shopping Cart is empty"
-  };
-   
- 
-  console.log("Current page is of type: " + page.type);
-   
-   
+   Ecwid.OnPageLoaded.add(function(page) {
+    if (page.type == "CART") {
+      // Print Page Type
+       console.log("Current page is of type: " + page.type);
+    }
 });
 </script>
