637c5f76f95492a4e18fc15f89d1b1032e6a82d6
SABA2023 <125206577+SABA2023@users.noreply.github.com>
2023-02-15 19:26:45 +0800

    Update sabasg.js

--- sabasg.js
+++ sabasg.js
@@ -1,13 +1,29 @@
 <script>
-   Ecwid.OnAPILoaded.add(function() {
-    console.log("Storefront JS API has loaded");
+ 
+
+Ecwid.OnPageLoaded.add(function(page){
+   	setTimeout(function hidePrice() {
+   		$(".ecwid-productBrowser-price").each(function(idx, el){
+           if ($(el).text() == Ecwid.formatCurrency(0)) {
+           		if(page.type == "CATEGORY") {
+           			$(el).text('Call for price');
+           			$('.ecwid-BuyNow-outOfStockLabel').hide();
+           		}
+           		else if (page.type == "PRODUCT") {
+  					$(this).text('Call for price');
+  					$('.ecwid-productBrowser-details-inStockLabel').show();
+  					$('.ecwid-productBrowser-details-outOfStockLabel').hide();
+  					$('.ecwid-AddToBagButton').hide();
+  				}
+            } 
+            else {
+            	$('.ecwid-productBrowser-details-outOfStockLabel').show();
+  				$('.ecwid-AddToBagButton').show();
+            }
+     	});
+   	}, 50);
 });
 
-   Ecwid.OnPageLoaded.add(function(page) {
-    if (page.type == "CART") {
-      // Print Page Type
-       console.log("Page Type: " + page.type);
-    }
-});
+
 
 </script>
